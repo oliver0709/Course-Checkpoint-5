277f2d939799ad571a251d99092dcdf310f05571
Oliver <150971104+oliver0709@users.noreply.github.com>
2024-06-11 03:34:14 +0200

    Create Project_Fizzbuzz.py

---
+++ Project_Fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz(max_num):
+    for num in range(1, max_num + 1):
+        if num % 3 == 0 and num % 5 == 0:
+            print('Fizzbuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+
+fizzbuzz(100)
